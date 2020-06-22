09cac1065b3f0f7d0f24d50a5d6b5c64fb4f6a23
Taibdse <buiductai232@gmail.com>
2020-06-22 22:05:16 +0700

    add antd

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-// import 'antd/dist/antd.css';
+import 'antd/dist/antd.css';
 import './App.css';
 import HomePage from './pages/HomePage';
 import GlobalContext from './context';
